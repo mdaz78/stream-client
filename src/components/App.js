f353efc0a8b2b98c638fbce7ebf2891b9db8a1bd
Md Abu Zafar <mdabuzafar78@gmail.com>
2021-01-02 21:55:35 +0530

    Add routing to different components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 
+import { HashRouter, Route } from 'react-router-dom';
+
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
+
 class App extends Component {
   render() {
-    return <h1>App</h1>;
+    return (
+      <>
+        <h1>Header</h1>
+        <HashRouter>
+          <>
+            <Route path='/' exact component={StreamList} />
+            <Route path='/streams/new' exact component={StreamCreate} />
+            <Route path='/streams/edit' exact component={StreamEdit} />
+            <Route path='/streams/delete' exact component={StreamDelete} />
+            <Route path='/streams/show' exact component={StreamShow} />
+          </>
+        </HashRouter>
+      </>
+    );
   }
 }
 
